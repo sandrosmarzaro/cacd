c419126b16267bc9f31045ab832a669d1686b6c3
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-11-30 00:04:19 -0300

    [ADDED] algoritmo de top autores

--- src/main/java/br/edu/ifes/cacd/AlgoritmoTopAutores.java
+++ src/main/java/br/edu/ifes/cacd/AlgoritmoTopAutores.java
@@ -1,4 +1,42 @@
 package br.edu.ifes.cacd;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class AlgoritmoTopAutores {
+
+    public static void run(Scanner scanner, Digrafo G) {
+        int option;
+        do {
+            Main.clearConsole();
+            System.out.println("d) Top Autores");
+            System.out.println("-".repeat(50));
+
+            Map<Integer, Integer> autoresPorQtdCitacoes  = new HashMap<>();
+
+            for (int artigoLido = 0; artigoLido < G.V(); artigoLido++) {
+                for (Aresta aresta : G.adj(artigoLido)) {
+                    int artigoCitado = aresta.getV2();
+                    int autorCitado = G.vertices().get(artigoCitado);
+                    if (!autoresPorQtdCitacoes.containsKey(autorCitado)) {
+                        autoresPorQtdCitacoes.put(autorCitado, 1);
+                    }
+                    else {
+                        int qtdCitacoes = autoresPorQtdCitacoes.get(autorCitado);
+                        autoresPorQtdCitacoes.put(autorCitado, qtdCitacoes + 1);
+                    }
+                }
+            }
+            for (Map.Entry<Integer, Integer> autor : autoresPorQtdCitacoes.entrySet()) {
+                System.out.println(autor.getKey() + " : " + autor.getValue());
+            }
+
+            System.out.println("-".repeat(50));
+            System.out.println("1 - Calcular Novamente\t\t0 - Voltar");
+            System.out.println("-".repeat(50));
+            System.out.print("Opção: ");
+            option = scanner.nextInt();
+        } while (option != 0);
+    }
 }
