8bce18a16c1d954d1f7cdb612e1f8982718aa5fe
Gabriel Cardoso de Avelar <gabri22kocebrac@gmail.com>
2022-12-07 20:48:11 -0300

    [UPDATE] added the option to recalculate (looping)

--- src/main/java/br/edu/ifes/cacd/AlgoritmoTopArtigos.java
+++ src/main/java/br/edu/ifes/cacd/AlgoritmoTopArtigos.java
@@ -1,26 +1,36 @@
 package br.edu.ifes.cacd;
 
+import java.util.Scanner;
+
 public class AlgoritmoTopArtigos {
 
-    public static void run(Scanner scanner, Digrafo G) {
+    public static void run(Scanner scanner,Digrafo G) {
         int option;
+        
+        do{
+            Main.clearConsole();
+            System.out.println("c) Top Artigos");
+            System.out.println("-----------------------------------------");
 
-        Main.clearConsole();
-        System.out.println("c) Top Artigos");
-        System.out.println("-".repeat(50));
+            int[] quantRefArtigos = new int[G.V()];
 
-        int[] quantRefArtigos = new int[G.V()];
-
-        for (int artigoLido = 0; artigoLido < G.V(); artigoLido++) {
-            for (Aresta aresta : G.adj(artigoLido)) {
-                int referencia = aresta.getV2();
-                quantRefArtigos[referencia]++;
+            for (int artigoLido = 0; artigoLido < G.V(); artigoLido++) {
+                for (Aresta aresta : G.adj(artigoLido)) {
+                    int referencia = aresta.getV2();
+                    quantRefArtigos[referencia]++;
+                }
             }
-        }
 
-        for(int x = 0; x < quantRefArtigos.lenght(); x++){
-            System.out.printf("Artigo[%d] : %d", x, quantRefArtigos[x]);
-        }
+            for(int x = 0; x < quantRefArtigos.length; x++){
+                System.out.printf("Artigo[%d] : %d\n", x, quantRefArtigos[x]);
+            }
+
+            System.out.println("-----------------------------------------");
+            System.out.println("1 - Calcular Novamente\t\t0 - Voltar");
+            System.out.println("-----------------------------------------");
+            System.out.print("Opção: ");
+            option = scanner.nextInt();
+        }while(option != 0);
     }
 }
 
