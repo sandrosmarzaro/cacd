cb0ba9fda31636ef63a31e1c60516c692a919a30
Sandro Smarzaro <sansmarzaro@gmail.com>
2022-12-08 00:57:24 -0300

    [ADDED] choose digraph file menu

--- src/main/java/br/edu/ifes/cacd/Main.java
+++ src/main/java/br/edu/ifes/cacd/Main.java
@@ -1,5 +1,7 @@
 package br.edu.ifes.cacd;
 
+import java.io.File;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -26,6 +28,7 @@ public class Main {
             System.out.println("2 - Algoritmo de Todos os Caminhos");
             System.out.println("3 - Algoritmo de Top Artigos");
             System.out.println("4 - Algoritmo de Top Autores");
+            System.out.println("5 - Escolher Dígrafo");
             System.out.println("0 - Sair");
             System.out.println("-".repeat(50));
             System.out.print("Escolha uma opção: ");
@@ -53,6 +56,7 @@ public class Main {
             case 2 -> AlgoritmoTodosCaminhos.run(scanner, G);
             case 3 -> AlgoritmoTopArtigos.run(scanner, G);
             case 4 -> AlgoritmoTopAutores.run(scanner, G);
+            case 5 -> chooseDigraph(scanner);
             default -> invalidOptionMenu();
         }
     }
@@ -80,6 +84,37 @@ public class Main {
             Thread.sleep(1250);
         } catch (Exception e) {
             System.out.println("Erro ao aguardar!");
+        }
+    }
+
+    private static void chooseDigraph(Scanner scanner) {
+        Main.clearConsole();
+        System.out.println("Escolha um dígrafo:");
+        System.out.println("-".repeat(50));
+        File folder = new File("./");
+        File[] listOfFiles = folder.listFiles();
+        try {
+            for (int i = 0; i < Objects.requireNonNull(listOfFiles).length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    System.out.println(i + 1 + " - " + listOfFiles[i].getName());
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Erro ao listar arquivos!");
+        }
+        System.out.println("0 - Voltar");
+        System.out.println("-".repeat(50));
+        System.out.print("Opção: ");
+        int option = scanner.nextInt();
+        if (option != 0) {
+            assert listOfFiles != null;
+            try {
+                Main.processDigraph(new String[]{listOfFiles[option - 1].getName()});
+            }
+            catch (Exception e) {
+                System.out.println("Erro ao processar dígrafo!");
+            }
         }
     }
 }
